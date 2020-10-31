86b5a54be5f993a32cd8aedc9ab38c60bbaf6093
Houssam Ouatmani <60886381+proservices-web@users.noreply.github.com>
2020-10-31 10:48:13 +0100

    calculator.java
    v1

---
+++ calculator.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static float addition (int p1, int p2){
+        return (p1 + p2);
+    }
+    static int subtraction(int p1, int p2){
+        return (p1 - p2);
+    }
+    static int multiplication (int p1, int p2){
+        return (p1 * p2);
+    }
+   static int division (int p1, int p2){
+        return (p1 / p2);
+    }
+    static int modulo (int p1, int p2){
+        return (p1 % p2);
+    }
+
+    public static void calc() throws InterruptedException {
+
+        System.out.println("Welcome to our calc loading ...");
+        Thread.sleep(1500);
+        var scanner = new Scanner(System.in);
+        System.out.println("Hey dude please enter a number\n");
+        int num1 = scanner.nextInt();
+        System.out.println("Hey dude please enter a number\n");
+        int num2 = scanner.nextInt();
+        System.out.println("Please enter an ops ; / * - + % ");
+        var ops = scanner.next();
+
+        switch (ops){
+            case "+":
+                System.out.println(addition(num1, num2));
+                break;
+            case "-":
+                System.out.println(subtraction(num1, num2));
+                break;
+            case "*":
+                System.out.println(multiplication(num1, num2));
+                break;
+            case "/":
+                System.out.println(division(num1, num2));
+                break;
+            case "%":
+                System.out.println(modulo(num1,num2));
+                break;
+            default:
+                System.out.println(addition(num1, num2));
+                System.out.println(subtraction(num1, num2));
+                System.out.println(multiplication(num1, num2));
+                System.out.println(division(num1, num2));
+                System.out.println(modulo(num1,num2));
+                break;
+
+
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        calc();
+    }
+}
